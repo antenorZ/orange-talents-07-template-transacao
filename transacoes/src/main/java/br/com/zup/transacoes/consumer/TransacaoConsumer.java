71fecbdc22325aeea5510d721bc34a503b68a53f
Marcos Antenor <marcos.souza@zup.com.br>
2021-09-09 15:26:19 -0300

    ajustes

--- transacoes/src/main/java/br/com/zup/transacoes/consumer/TransacaoConsumer.java
+++ transacoes/src/main/java/br/com/zup/transacoes/consumer/TransacaoConsumer.java
@@ -1,11 +1,15 @@
 package br.com.zup.transacoes.consumer;
 
 import br.com.zup.transacoes.dto.TransacaoDto;
+import br.com.zup.transacoes.model.Transacao;
+import br.com.zup.transacoes.repository.TransacaoRepository;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,12 +23,17 @@ public class TransacaoConsumer {
     @Value(value = "${spring.kafka.consumer.group-id}")
     private String groupId;
 
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+
     @KafkaListener(topics = "${spring.kafka.topic.transactions}",
             groupId = "${spring.kafka.consumer.group-id}",
             containerFactory = "transacoesKafkaListenerContainerFactory")
-    public void ouveTopicoTransacoes(ConsumerRecord<String, TransacaoDto> record){
-        log.info("Mensagem Recebida" + record.partition());
-        log.info("Mensagem Recebida" + record.value());
+    public void ouveTopicoTransacoes(ConsumerRecord<String, TransacaoDto> record, @Payload TransacaoDto transacaoDto){
+        log.info("Mensagem Recebida - Partição: " + record.partition());
+        log.info("Mensagem Recebida - Valor: " + record.value());
+        Transacao transacao = transacaoDto.toModel();
+        transacaoRepository.save(transacao);
     }
 
 }
---
+++ transacoes/src/main/java/br/com/zup/transacoes/repository/TransacaoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.transacoes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransacaoRepository extends JpaRepository {
+}
